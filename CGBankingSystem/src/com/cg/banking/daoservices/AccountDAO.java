b1c5593cad2d5aec9132051c09fc5648b8aa5ebf
SaiyamLunia <s.lunia59@gmail.com>
2019-02-09 16:18:54 +0530

    Initial Upload

--- CGBankingSystem/src/com/cg/banking/daoservices/AccountDAO.java
+++
@@ -1,9 +0,0 @@
-package com.cg.banking.daoservices;
-import java.util.List;
-import com.cg.banking.beans.Account;
-public interface AccountDAO {
-	Account save(Account account);
-	boolean update(Account account);
-	Account findOne(int accountNo);
-	List<Account> findAll();
-}
--- CGBankingSystem/src/com/cg/banking/daoservices/TransactionDAO.java
+++
@@ -1,8 +0,0 @@
-package com.cg.banking.daoservices;
-import java.util.List;
-import com.cg.banking.beans.Transaction;
-public interface TransactionDAO {
-	Transaction save(int accountNumber, Transaction transaction);
-	Transaction findOne(int accountNumber, int transactionId);
-	List<Transaction> findAll(int accountNumber);
-}
